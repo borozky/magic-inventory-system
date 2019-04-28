0947f576ca8c71304509a0a50c7e90a7af57ea43
Joshua Orozco <borozky@gmail.com>
2019-04-28 14:38:45 +1000

    Added revised version of original project

---
+++ MagicInventorySystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagicInventorySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
